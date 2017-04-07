1489d242eb79072a55b889f0e736e4099b4a5802
arunan <arunans23@gmail.com>
2017-04-07 23:47:09 +0530

    display text file data in chart

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
   title = 'app works!';
+
+  private pm25data :number[] = [];
+  private pm10data :number[] = [];
+
+  private currentText :string = "";
+  private currentPM25Sum :number = 0;
+  private currentPM10Sum :number = 0;
+
   // lineChart
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'PM2.5'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'PM10'}
+     {data: [0, 0, 0, 0, 0, 0, 0], label: 'PM2.5'}
+    //{data: this.pm25data, label: 'PM2.5'}
+     //{data: [28, 48, 40, 19, 86, 27, 90], label: 'PM10'}
+    //{data: this.pm10data, label: 'PM10'}
   ];
   public lineChartLabels:Array<any> = ['10am', '12noon', '2pm', '4pm', '6pm', '8pm'];
   public lineChartOptions:any = {
@@ -71,9 +81,33 @@ export class HomePageComponent {
       reader.onload = () => {
         // this 'text' is the content of the file
         var text = reader.result;
-        console.log(text);
+        //console.log(((text.split("\n"))[0].split(' '))[0]);
+
+        const textArray = text.split("\n");
+
+        for (var i = 0; i < 6; i++){
+          this.currentPM25Sum = 0;
+          this.currentPM10Sum = 0;
+          for (var m = 0; m < 10; m++){
+            this.currentText= textArray[10 * i + m];
+            this.currentPM25Sum += parseInt((this.currentText.split(' '))[2]);
+            //this.currentPM10Sum += parseInt((this.currentText.split(' ' || '\t\t'))[5]);
+            //console.logPM25((this.currentText.split(' '))[2]);
+          }
+          console.log(this.currentPM25Sum/100);
+          this.pm25data.push(this.currentPM25Sum/100);
+          this.pm10data.push(this.currentPM10Sum/100);
+        }
       }
       reader.readAsText(input.files[index]);
     }
+  }
+
+  update(){
+    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+    for (let i = 0; i < this.lineChartData.length; i++) {
+      _lineChartData[i] = {data: this.pm25data, label: this.lineChartData[i].label};
+    }
+    this.lineChartData = _lineChartData;
   }
 }
